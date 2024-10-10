fde08ce1ec365bacc6dd9be05b919e8292de527c
Vini <vouverney27@gmail.com>
2024-10-10 10:49:42 -0300

    Getting more concepts

--- javascript/react/creating-components/src/App.js
+++ javascript/react/creating-components/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(){
-  return(
-    <h1>Hello World!</h1>
-  );
-}
+import Header from './components/Header'
+import Nav from './components/Nav'
+import Intro1 from './components/Intro1'
+import Intro2 from './components/Intro2'
+import Footer from './components/Footer'
 
 function App() {
-  return <Header/>
+  return(
+    <div>
+      <Header name='Peter'/>
+      <Nav />
+      <Intro1 />
+      <Intro2 />
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
